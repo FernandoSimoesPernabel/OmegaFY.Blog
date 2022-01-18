3f290bc031295e79c14cc306a4d0ab6830e2c48f
Fernando Simões Pernabel <f_pernabel@hotmail.com>
2022-01-18 09:24:29 -0300

    refactor: Alterado UnitOfWorkBehavior para so executar para ICommand

--- src/OmegaFY.Blog.Application/PipelineBehaviors/UnitOfWorkBehavior.cs
+++ src/OmegaFY.Blog.Application/PipelineBehaviors/UnitOfWorkBehavior.cs
@@ -4,21 +4,20 @@ using OmegaFY.Blog.Domain.Repositories;
 
 namespace OmegaFY.Blog.Application.PipelineBehaviors;
 
-public class UnitOfWorkBehavior<TRequest, TResult> : IPipelineBehavior<TRequest, TResult>
+public class UnitOfWorkBehavior<TCommand, TResult> : IPipelineBehavior<TCommand, TResult> where TCommand : ICommand
 {
 
     private readonly IUnitOfWork _unitOfWork;
 
     public UnitOfWorkBehavior(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
 
-    public async Task<TResult> Handle(TRequest request,
+    public async Task<TResult> Handle(TCommand request,
                                       CancellationToken cancellationToken,
                                       RequestHandlerDelegate<TResult> next)
     {
         TResult response = await next();
 
-        if (request is ICommand)
-            await _unitOfWork.SaveChangesAsync();
+        await _unitOfWork.SaveChangesAsync();
 
         return response;
     }
