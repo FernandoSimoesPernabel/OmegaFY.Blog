de82604b52f724cd8ac8439f6e7a4b6f88359bff
Fernando Simões Pernabel <f_pernabel@hotmail.com>
2022-06-01 14:26:48 -0300

    fix (data/user): Criado migration do index unique para usuarios

--- src/OmegaFY.Blog.Data.EF/Migrations/Users/20220601172541_Add_Email_Index_Unique.Designer.cs
+++ src/OmegaFY.Blog.Data.EF/Migrations/Users/20220601172541_Add_Email_Index_Unique.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OmegaFY.Blog.Data.EF.Context;
 
@@ -10,9 +11,10 @@ using OmegaFY.Blog.Data.EF.Context;
 namespace OmegaFY.Blog.Data.EF.Migrations.Users
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20220601172541_Add_Email_Index_Unique")]
+    partial class Add_Email_Index_Unique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.5");
