95a0c3865b0b246b2244858ce09c9d43258d26a6
Fernando Simões Pernabel <f_pernabel@hotmail.com>
2025-01-30 11:25:41 -0300

    feat: Criado background services

--- src/OmegaFY.Blog.Data.MongoDB/Extensions/PostBasicInfoCollectionModelExtensions.cs
+++ src/OmegaFY.Blog.Data.MongoDB/Extensions/PostBasicInfoCollectionModelExtensions.cs
@@ -3,7 +3,7 @@ using OmegaFY.Blog.Domain.Entities.Posts;
 
 namespace OmegaFY.Blog.Data.MongoDB.Extensions;
 
-public static class PostCollectionModelExtensions
+public static class PostBasicInfoCollectionModelExtensions
 {
     public static Post ToPost(this PostBasicInfoCollectionModel postModel) 
         => Post.Create(postModel.Id, postModel.AuthorId, postModel.Header, postModel.Body, postModel.ModificationDetails, postModel.Private);
---
+++ src/OmegaFY.Blog.WebAPI/BackgroundServices/ThreadsPerformanceMonitoringService.cs
@@ -0,0 +1,42 @@
+﻿namespace OmegaFY.Blog.WebAPI.BackgroundServices;
+
+internal sealed class ThreadsPerformanceMonitoringService : BackgroundService
+{
+    private readonly ILogger<ThreadsPerformanceMonitoringService> _logger;
+
+    public ThreadsPerformanceMonitoringService(ILogger<ThreadsPerformanceMonitoringService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("GC and Thread Monitoring Service is starting.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                _logger.LogInformation("=== ThreadPool Metrics ===");
+
+                ThreadPool.GetAvailableThreads(out int availableWorkerThreads, out int availableCompletionPortThreads);
+
+                ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxCompletionPortThreads);
+
+                ThreadPool.GetMinThreads(out int minWorkerThreads, out int minCompletionPortThreads);
+
+                _logger.LogInformation("Available Worker Threads: {availableWorkerThreads}/{maxWorkerThreads} (Min: {minWorkerThreads})", availableWorkerThreads, maxWorkerThreads, minWorkerThreads);
+                
+                _logger.LogInformation("Available Completion Port Threads: {availableCompletionPortThreads}/{maxCompletionPortThreads} (Min: {minCompletionPortThreads})", availableCompletionPortThreads, maxCompletionPortThreads, minCompletionPortThreads);
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro no serviço de monitoramento de ThreadPool.");
+            }
+        }
+
+        _logger.LogInformation("Thread Monitoring Service is stopping.");
+    }
+}
